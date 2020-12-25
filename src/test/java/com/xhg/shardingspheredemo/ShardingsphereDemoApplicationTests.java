455fab48180752bf4e054cc0de57b595fdd5890d
objectxhg <857414521@qq.com>
2020-12-25 17:41:50 +0800

    first commit

---
+++ src/test/java/com/xhg/shardingspheredemo/ShardingsphereDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhg.shardingspheredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
